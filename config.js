54c1dd0e64763e175318f9c54ed24aef726dbef1
frank.96lol@gmail.com <frank.96lol@gmail.com>
2022-12-27 17:44:25 -0500

    Fixed Minnor Bugs

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
  * Config Main File *
  ********************/
 module.exports = {
+    PORT: process.env.PORT || 3000,
     DIR: __dirname,
     CLI: __dirname + "/client",
     LOGIC: __dirname + "/server/logic",
--- server/logic/router.js
+++ server/logic/router.js
@@ -5,7 +5,7 @@
 const config = require("../../config.js");
 const router = require("express").Router();
 const index = async (req , res) => {
-    res.sendFile(config.CLI + "index.html");
+    res.sendFile(config.CLI + "/index.html");
 };
 
 router.use("/", index);
