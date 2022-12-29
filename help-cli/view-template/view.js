83397ff71862f475bf2bd856eea4e56feeabda37
Rodny Estrada <rrodnyestrada1@gmail.com>
2022-12-28 22:08:03 -0500

    Fixed template view js bug

--- help-cli/view-template/view.js
+++ help-cli/view-template/view.js
@@ -7,7 +7,7 @@ let {{NAME}}View;
 function init{{UPNAME}}View () {
     
     {{NAME}}View = new ViewComponent("#{{NAME}}-view");
-    authView.element.innerText = "{{UPNAME}}";
+    {{NAME}}View.element.innerText = "{{UPNAME}}";
     
     init{{UPNAME}}View = null;
     console.log("{{NAME}}-view ready!");
