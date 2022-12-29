3530ae513a6ec9cda59b9be311611a4f361ec7dd
Rodny Estrada <rrodnyestrada1@gmail.com>
2022-12-28 23:16:18 -0500

    Added modal to force landscape screen mode

--- client/js/main.js
+++ client/js/main.js
@@ -12,5 +12,23 @@ function main () {
     initMenuView();
     initGameView();
     
+    
+    // force landscape
+    const landscapeModal = new ModalComponent("#landscape-modal");
+    setInterval(() => {
+        if (getOrientation() === "landscape") landscapeModal.hide();
+        else landscapeModal.show();
+    }, 200);
+    
+    
     console.log("ready!");
+}
+
+
+/**
+ * Get screen orientation
+ * @return String - "landscape" or "portrait"
+ */
+function getOrientation () {
+    return innerWidth > innerHeight ? "landscape" : "portrait";
 }
